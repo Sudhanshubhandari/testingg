2e2489ac6297e97b1bbd94e9a91dde58f40b32b7
Sudhanshu <ashish>
2025-02-05 16:18:23 +0530

    pusheddddd d d d d d d

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,8 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/particular-validation/*" element={<ParticularValidation />} />
+        <Route path="/particular-validation" element={<ParticularValidation />} />
         <Route path="/profile-validation" element={<ProfileValidation />} />
-        {/* Catch all route - redirect to home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
